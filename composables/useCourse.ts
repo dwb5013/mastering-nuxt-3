dce07acd4ceb0f00c5dbf11759f02506634385a0
digika-ding <weibing.ding@digika.jp>
2023-12-03 22:26:32 +0900

    add type

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,13 +1,34 @@
 import courseData from "./courseData"
+type Lesson = {
+  title: string
+  slug: string
+  number: number
+  downloadUrl:
+  string
+  videoId: number
+  text: string
+  sourceUrl?: string
+
+}
+
+type Chapter = {
+  title: string
+  slug: string
+  number: number
+  lessons: Lesson[]
+}
+
+type Course = {
+  title: string
+  chapters: Chapter[]
+}
 export const useCourse = () => {
-  return {
-    ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
-  }
+  const chapters: Chapter[] = courseData.chapters.map((chapter) => {
+    const lessons = chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`
+    }))
+    return { ...chapter, lessons }
+  })
+  return { ...courseData, chapters }
 }
--- pages/course.vue
+++ pages/course.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
-        </span>
-      </h1>
-    </div>
+  <div class="prose mb-12">
+    <h1>
+      <span class="font-medium">
+        Course:
+        <span class="font-bold">Mastering Nuxt 3</span>
+      </span>
+    </h1>
 
     <div class="flex flex-row justify-center flex-grow">
       <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col">

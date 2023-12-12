1454f2c6d500276a78f92fb9ef039251e06b568c
digika-ding <weibing.ding@digika.jp>
2023-12-12 22:43:08 +0900

    feat
    login with 0Auth

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,28 @@
 <template>
-    <div class="prose w-full max-w-2xl h-9">
-        <h1>Log in to {{ title }}</h1>
-        <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
-            Log in with Github
-        </button>
-    </div>
+  <div class="prose w-full max-w-2xl h-9">
+    <h1>Log in to {{ title }}</h1>
+    <button
+      class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      @click="signInWithOAuth"
+    >
+      Log in with Github
+    </button>
+  </div>
 </template>
-  
+
 <script setup lang="ts">
 const { title } = useCourse();
+const supabase = useSupabaseClient();
+
+const signInWithOAuth = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+  if (error) console.log(error);
+};
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) console.log(error);
+};
 </script>
